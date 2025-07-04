d75e07855e1ffcc8e2e54319e355b5f1a91acd6e
Bharat Verma <118604804+Bharat-vivek@users.noreply.github.com>
2025-07-04 20:00:31 +0530

    added pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../index.css'
+
+const Home = () => {
+    return (
+        <div className='bg-red-500'>Home
+
+
+        </div>
+
+    )
+}
+
+export default Home
